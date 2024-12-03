5a5e991af132f9d1b9881118272ed975c5be8982
Alex Campbell <orionkerman@gmail.com>
2024-12-03 14:26:46 -0600

    Changed port

--- 0_AvionicsnDAQ/Datalogger/TCPtestprog.py
+++ 0_AvionicsnDAQ/Datalogger/TCPtestprog.py
@@ -4,7 +4,7 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-server_address = ('localhost', 5020)
+server_address = ('localhost', 65432)
 server_socket.bind(server_address)
 
 # Enable the server to accept connections
