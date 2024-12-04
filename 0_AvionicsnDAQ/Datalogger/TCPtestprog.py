c7423a1f827fa37ee34d3822b2234cb249d4f0c6
Alex Campbell <orionkerman@gmail.com>
2024-12-04 14:51:20 -0600

    Added packet size handling code

--- 0_AvionicsnDAQ/Datalogger/TCPtestprog.py
+++ 0_AvionicsnDAQ/Datalogger/TCPtestprog.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # Define the host and port for the server
 host = '0.0.0.0'  # Listen on all available interfaces (use specific IP for remote access)
-port = 2869     # Port to listen on (ensure it's open and available)
+port = 2870     # Port to listen on (ensure it's open and available)
 
 # Create a TCP/IP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
